a3d45282c5f44991f9e9b7f5ccf56a9ff4dad2b1
Isabella Mills <isabellamills04@gmail.com>
2024-10-31 17:43:17 -0500

    contact and html page css

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ File Name: script.js
 Date: 10/17/2024
 */
 
+//Functions for navigation
+
 //Hamburger menu function
 function hamburger() {
     var menu = document.getElementById("menu-links");
@@ -15,14 +17,133 @@ function hamburger() {
     }
 }
 
-////Service description display funtion
-//function basicBathBrush() {
-//    var service = document.getElementById("basic-bath-brush");
-//    if (service.style.display === none) {
-//        service.style.display = block;
-//    }
-//    else {
-//        service.style.display = none;
-//    }
-//    
-//}
+//Functions for Service Page
+
+//Basic bath and brush function
+function bathBrush() {
+    var info = document.getElementById("bath-and-brush");
+    var title = document.getElementById("service1-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Full grooming function
+function fullGrooming() {
+    var info = document.getElementById("full-grooming");
+    var title = document.getElementById("service2-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Puppy's first groom function
+function firstGroom() {
+    var info = document.getElementById("first-groom");
+    var title = document.getElementById("service3-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Deshedding treatment function
+function deshedding() {
+    var info = document.getElementById("deshedding");
+    var title = document.getElementById("service4-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Deluxe spa package function
+function deluxeSpa() {
+    var info = document.getElementById("deluxe-spa");
+    var title = document.getElementById("service5-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Add-on services function
+function addOns() {
+    var info = document.getElementById("add-ons");
+    var title = document.getElementById("service6-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Pet bakery function
+function petBakery() {
+    var info = document.getElementById("pet-bakery");
+    var title = document.getElementById("service7-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Grooming boutique function
+function groomingBoutique() {
+    var info = document.getElementById("grooming-boutique");
+    var title = document.getElementById("service8-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+//Doggie daycare function
+function doggieDaycare() {
+    var info = document.getElementById("doggie-daycare");
+    var title = document.getElementById("service9-title");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        title.style.display = "none";
+    }
+    else {
+        info.style.display = "none";
+        title.style.display = "block";
+    }
+}
+
+
+
